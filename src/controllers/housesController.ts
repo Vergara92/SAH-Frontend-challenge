0d6c22b065640afcdedfb588ad3d1aeeb3e99da3
Vergara <vergara.gonzalez@hotmail.es>
2021-10-16 21:14:43 +0200

    perf(sah-challenge): :zap: Add CORS and limit houses to 50

--- src/controllers/housesController.ts
+++ src/controllers/housesController.ts
@@ -1,10 +1,19 @@
 import getHouses from "../api/getHouses"
 import House from "../models/House"
+import IHouse from "../models/IHouse.interface"
 
 export default class housesController {
   async getList() {
-    const houseList = await getHouses()
+    const rawhouseList = await getHouses()
+    const modeledHouseList: IHouse[] = []
 
-    return houseList.map(house => new House(house))
+    for (let index = 0; index < rawhouseList.length; index++) {
+      const rawHouse = rawhouseList[index]
+      if(index >= 50) break
+
+      modeledHouseList.push(new House(rawHouse))
+    }
+
+    return modeledHouseList
   }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,21 @@
 import express from 'express'
+import cors from 'cors'
 import housesController from '../controllers/housesController'
 
 const router = express.Router()
+
+const corsOptions: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  preflightContinue: false,
+}
+
+router.use(cors(corsOptions))
 
 router.get('/get-houses', async (_req, res) => {
   const controller = new housesController()
